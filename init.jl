fd3dfa0df19a21d006b8f2ea5e0d76fd19c2b264
Tanmay Thakur <thakurtanmay72@yahoo.com>
2020-02-11 00:11:27 +0530

    Arrays and types (abstract and concrete) add

--- init.jl
+++ init.jl
@@ -16,3 +16,16 @@ println("Hello!" * " My word!")  # strings are combined using *
 println("Hello!" * " My word!"^3) # strings are repeated using ^
 
 # Strings are immutable in Julia
+
+abstypevariable = Array(Integer, 2,3) # A two-dimensional array with 2 rows and 3 columns
+
+abstypevariable[2,1]    #NB --- note the brackets, that's how to access elements of an array
+
+abstypevariable[1,1] = 1    # Int64 is a subtype of Integer
+
+abstypevariable # Note that abstypevariable is still abstract, even if it contains an element of definite type"
+
+abstypevariable[1,2] = 5.0  # given value is of type Float64 although when defined it is type converted to Int64
+
+arbconcretevariable = Array(Int64, 2,3)  # no actual value specified,
+#                                             ... so Julia assigns arbitrary values
